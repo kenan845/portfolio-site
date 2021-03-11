e8079c911215b1111ee04d8ffda255cb50c6e72a
kenan845 <79484155+kenan845@users.noreply.github.com>
2021-03-10 21:56:12 -0500

    buttons added to website

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+function assignment1(){
+alert("assignment 1 coming soon");
+  }
+
+function assignment2(){
+alert("assignment 2 coming soon");
+  }
+
+function assignment3(){
+alert("assignment 3 coming soon");
+  }
+          
+function assignment4(){
+alert("assignment 4 coming soon");
+  }
+
+function assignment5(){
+alert("assignment 5 coming soon");
+  }
+
+function assignment6(){
+alert("assignment 6 coming soon");
+  }
+
+function assignment7(){
+alert("assignment 7 coming soon");
+  }
+
+function assignment8(){
+alert("assignment 8 coming soon");
+  }
+
+function assignment9(){
+alert("assignment 9 coming soon");
+  }
